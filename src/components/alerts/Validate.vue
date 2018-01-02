d4cd619a492fe3be12761d2a29b960278f7482a7
Vinicius Azevedo <viniazvd@gmail.com>
2018-01-02 19:17:09 -0200

    in construction

---
+++ src/components/alerts/Validate.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-alert
+    class="my-alert"
+    color="negative"
+    icon="warning"
+    position="bottom"
+    dismissible
+    v-model="this.isActived"
+  >
+    Dado inválido
+  </q-alert>
+</template>
+
+<script>
+import QAlert from 'quasar'
+
+export default {
+  name: 'alert-validate',
+  props: ['isActived'],
+  components: { QAlert }
+}
+</script>
+
+<style scoped>
+.my-alert {
+  margin-left: 448px;
+  margin-right: 446px;
+  margin-bottom: 115px;
+}
+</style>
