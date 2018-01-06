f369023bf8449b99a93b394d57a9b78bb23f4e11
Vinicius Azevedo <viniazvd@gmail.com>
2018-01-06 20:33:21 -0200

    created

---
+++ src/app/auth/components/inputs/InputPassword.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-field icon="lock">
+    <q-input
+      type="password"
+      float-label="Senha"
+      :value="this.password" 
+      @input="this.emitValue"
+      clearable
+      @keyup.enter="emitKeyUp" 
+    />
+  </q-field>
+</template>
+
+<script>
+import { QField, QInput } from 'quasar'
+
+export default {
+  name: 'inputPassword',
+
+  props: ['password'],
+
+  components: { QField, QInput },
+
+  methods: {
+    emitValue (value) {
+      this.$emit('update:password', value)
+    },
+    emitKeyUp () {
+      this.$emit('keyUp')
+    }
+  }
+}
+</script>
