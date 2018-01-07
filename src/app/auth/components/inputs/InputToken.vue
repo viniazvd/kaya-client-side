d1356f547ef49783fd6eba94398f77f5fc6fcadf
Vinicius Azevedo <viniazvd@gmail.com>
2018-01-07 14:13:53 -0200

    created

---
+++ src/app/auth/components/inputs/InputToken.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-field icon="vpn_key">
+    <q-input
+      type="text"
+      float-label="Token recebido pelo e-mail"
+      :value="this.token" 
+      @input="this.emitValue"
+      clearable
+      @keyup.enter="emitKeyUp" 
+    />
+  </q-field>
+</template>
+
+<script>
+import { QField, QInput } from 'quasar'
+
+export default {
+  name: 'inputToken',
+
+  props: ['token'],
+
+  components: { QField, QInput },
+
+  methods: {
+    emitValue (value) {
+      this.$emit('update:token', value)
+    },
+    emitKeyUp () {
+      this.$emit('keyUp')
+    }
+  }
+}
+</script>
