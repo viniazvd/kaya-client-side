7b1210fc00c84a03b424b5971ae76784b98c82d4
Vinicius Azevedo <viniazvd@gmail.com>
2018-01-09 12:49:00 -0200

    new routes

--- src/app/auth/routes.js
+++ src/app/auth/routes.js
@@ -1,5 +1,11 @@
-import Main from './components/Main'
+import Login from './components/Login'
+import ForgotPassword from './components/ForgotPassword'
+import ForgotEmail from './components/ForgotEmail'
+import CreateAccount from './components/CreateAccount'
 
 export default [
-  { path: '/auth', component: Main }
+  { path: '/login', component: Login, meta: { requiresAuth: false } },
+  { path: '/forgotPassword', component: ForgotPassword, meta: { requiresAuth: false } },
+  { path: '/forgotEmail', component: ForgotEmail, meta: { requiresAuth: false } },
+  { path: '/createAccount', component: CreateAccount, meta: { requiresAuth: false } }
 ]
