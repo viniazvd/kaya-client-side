1e2f769532c832b7629888959fcad4ef2f55c771
Vinicius Azevedo <viniazvd@gmail.com>
2018-01-11 14:53:01 -0200

    removing getEmails (moved to user domains)

--- src/domains/auth/services/index.js
+++ src/domains/auth/services/index.js
@@ -14,11 +14,6 @@ export const getEmailToken = email => {
   // .then(response => response.data.result.msg)
 }
 
-export const getEmails = () => {
-  return http.get('user')
-    .then(response => response.data.results.users)
-}
-
 export const postNewPassword = ({ token, email, newPassword }) => {
   return http.post('auth/change-password', { token, email, newPassword })
 }
