3a32679d7b88183ea0fdcedd17d4c5bb6de34327
Vinicius Azevedo <viniazvd@gmail.com>
2018-01-11 03:07:19 -0200

    moved loader logic here

--- src/Root.vue
+++ src/Root.vue
@@ -8,6 +8,10 @@
       </a>
     </div>
 
+    <q-inner-loading :visible="this.showLoader">
+      <q-spinner-gears size="300px" color="primary" />
+    </q-inner-loading>
+
     <router-view v-if="!this.isAuthenticated()" />
   </div>
 </template>
@@ -15,14 +19,21 @@
 <script>
 import { mapGetters } from 'vuex'
 import Navbar from './components/navbar/Main'
+import { QInnerLoading, QSpinnerGears } from 'quasar'
 
 export default {
   name: 'root',
 
-  components: { Navbar },
+  components: { Navbar, QInnerLoading, QSpinnerGears },
 
   methods: {
     ...mapGetters(['isAuthenticated'])
+  },
+
+  computed: {
+    showLoader () {
+      return this.$store.state.loader.loading
+    }
   }
 }
 </script>
